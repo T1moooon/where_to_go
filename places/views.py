b00826b689756e1708306a56e16c875ae544efc4
Timon <74593747+T1moooon@users.noreply.github.com>
2025-07-31 23:38:18 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Place
 
-# Create your views here.
+
+def show_places(request):
+    places = Place.objects.all()
+    geojson_places = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [float(place.longitude), float(place.latitude)]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": f"/places/{place.id}/json/"
+                }
+            } for place in places
+        ]
+    }
+    return render(request, 'index.html', {'geojson_places': geojson_places})
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from where_to_go import views
+from places.views import show_places
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', show_places)
 ]
 
 if settings.DEBUG:
