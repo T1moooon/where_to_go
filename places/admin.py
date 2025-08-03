8a35698f8e3742d2eb2e1d992eae60c99d972f5a
Timon <74593747+T1moooon@users.noreply.github.com>
2025-08-03 16:41:04 +0300

    add preview to admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, PlaceImage
 
 
 class PlaceImageInLine(admin.TabularInline):
     model = PlaceImage
     extra = 1
-    fields = ('image', 'position')
+    fields = ('image', 'previews', 'position')
+    readonly_fields = ('previews',)
+
+    def previews(self, obj):
+        if obj.pk and obj.image:
+            return format_html(
+                "<img src='{}' style='max-width: 300px; max-height: 200px;'/>",
+                obj.image.url
+            )
+        return 'Нет файла'
 
 
 @admin.register(Place)
