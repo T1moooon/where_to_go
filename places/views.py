19075a824685f6b3426e4470ba78fd82c1ce702f
Timon <74593747+T1moooon@users.noreply.github.com>
2025-08-03 14:09:17 +0300

    uploaded json

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Place
 
 
@@ -22,3 +23,18 @@ def show_places(request):
         ]
     }
     return render(request, 'index.html', {'geojson_places': geojson_places})
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    imgs = [img.image.url for img in place.images.all().order_by('order')]
+    return JsonResponse({
+        'title': place.title,
+        'imgs': imgs,
+        'short_description': place.description_short,
+        'long_description': place.description_long,
+        'coordinates': {
+            'lat': float(place.latitude),
+            'lng': float(place.longitude)
+        }
+    }, json_dumps_params={'ensure_ascii': False, 'indent': 2})
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from places.views import show_places
+from places.views import show_places, place_detail
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_places)
+    path('', show_places),
+    path('places/<int:place_id>/', place_detail)
 ]
 
 if settings.DEBUG:
